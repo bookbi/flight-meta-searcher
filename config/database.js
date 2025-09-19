df080e7ec47c995df4ddd577a7f08b8d50ca4162
BOOKBIG <yanapatinkham@gmail.com>
2025-09-19 21:57:16 +0700

    Update MAin
    เนื้อหา main เพิ่มเติม

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize(
   'airport_db',
   'postgres',
-  'your_password', // replace with your actual password
+  '262005', // replace with your actual password
   {
     host: 'localhost',
     dialect: 'postgres'
